afb37ff82ff4f12d4f0809e29fc9ce3a7fc12d40
chriss1525 <okothchris15@gmail.com>
2023-06-23 14:07:44 +0300

    create a puppet file and give ownership

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create new file in tmp folder
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
