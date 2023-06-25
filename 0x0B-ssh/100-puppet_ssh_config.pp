84041085661d981053cd7682be032a99596816b2
chriss1525 <okothchris15@gmail.com>
2023-06-26 01:34:25 +0300

    set up client SSH configuration file to connect to a server without typing a password

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# set up client SSH config file to connect a server without typing a password.
+include stdlib
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
