03d89f0ab3a61b0e709f3028974639087598749c
chriss1525 <okothchris15@gmail.com>
2023-08-11 01:44:54 +0300

    query the reddit api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ return number of subscribers """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
@@ -13,6 +14,7 @@ def number_of_subscribers(subreddit):
     if r.status_code == 404:
         return 0
     return r.json().get('data').get('subscribers')
+
 
 if __name__ == '__main__':
     number_of_subscribers(subreddit)
